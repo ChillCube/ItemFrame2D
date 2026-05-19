20bedae17290cb5482f1b7ac73e7968f5689268b
Your Name <franzluehr@gmail.com>
2026-05-19 11:34:23 +0200

    docs: auto-sync v6.8.0

--- itemframe.gd
+++ itemframe.gd
@@ -3,24 +3,22 @@
 extends SmoothUI
 class_name ItemFrame2D
 
-@export var item : Item;
-@export var item_size : float = 1;
-var item_sprite : Sprite2D;
+@export var item : Item:
+	set(val):
+		item = val
+		queue_redraw()
 
-func _ensure_sprite():
-	if !item_sprite:
-		item_sprite = Sprite2D.new();
-		add_child(item_sprite)
-		item_sprite.global_position = global_position;
+@export var item_size : float = 1:
+	set(val):
+		item_size = val
+		queue_redraw()
 
 func _process(delta: float) -> void:
 	super(delta)
-	_ensure_sprite();
-	item_sprite.global_position = global_position;
-	if item:
-		if item.sprite and item_sprite:
-			item_sprite.texture = item.sprite;
-			item_sprite.scale = Vector2(item_size, item_size);
-	else:
-		if item_sprite:
-			item_sprite.texture = null;
+	if Engine.is_editor_hint():
+		queue_redraw()
+
+func _draw() -> void:
+	if item and item.sprite:
+		var size := item.sprite.get_size() * item_size
+		draw_texture_rect(item.sprite, Rect2(-size / 2.0, size), false)
